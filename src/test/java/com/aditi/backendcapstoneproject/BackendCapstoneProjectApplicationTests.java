6d9c462782b3e5ec9b9c199085bca044ba9518a5
Aditi <rawataditi81@gmail.com>
2026-01-17 22:30:24 +0530

    Updated BackendCapstoneProjectApplicationTests.java

--- src/test/java/com/aditi/backendcapstoneproject/BackendCapstoneProjectApplicationTests.java
+++ src/test/java/com/aditi/backendcapstoneproject/BackendCapstoneProjectApplicationTests.java
@@ -2,8 +2,10 @@ package com.aditi.backendcapstoneproject;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BackendCapstoneProjectApplicationTests {
 
     @Test
